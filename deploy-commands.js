d544f502264dc2b726b10531480d67e1b0f81f49
Kiga-Kagemasu <a94447771@gmail.com>
2025-06-21 21:25:43 +0900

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,6 @@
-const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = process.env;
 const fs = require('fs');
-const path = require('path');
+const { REST, Routes } = require('discord.js');
+require('dotenv').config();
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -11,16 +10,16 @@ for (const file of commandFiles) {
   commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    console.log('スラッシュコマンドを登録中...');
+    console.log('スラッシュコマンドを登録中（ギルド限定）...');
     await rest.put(
-      Routes.applicationGuildCommands(clientId, guildId),
-      { body: commands }
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands },
     );
-    console.log('登録完了。');
+    console.log('スラッシュコマンドの登録が完了しました！（即時反映）');
   } catch (error) {
     console.error(error);
   }
