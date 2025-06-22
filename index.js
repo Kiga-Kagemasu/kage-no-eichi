19e4a60e0f07e08a5cfb7923e3c290a91475efb1
Kiga-Kagemasu <a94447771@gmail.com>
2025-06-22 18:59:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Bot is running!'));
+app.listen(3000, () => console.log('Web server is ready.'));
+
 const fs = require('fs');
 const path = require('path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
