330b2c49a0da68b1db3e24352c3ffec357d371ea
Kiga-Kagemasu <a94447771@gmail.com>
2025-06-27 02:32:56 +0900

    Update help.js

--- app/commands/help.js
+++ app/commands/help.js
@@ -7,21 +7,32 @@ module.exports = {
 
   async execute(interaction) {
     const embed = new EmbedBuilder()
-      .setTitle('コマンド一覧')
-      .setDescription('各カテゴリごとにBotのコマンドを紹介します。')
-      .setColor(0x00BFFF) // 色
+      .setTitle('コマンド 使い方')
+      .setDescription('各コマンドを紹介します。') 
+      .setColor(0x00BFFF)
       .addFields(
         {
-          name: 'コマンド一覧',
-          value:
-            '`/キャラ検索` - 正式名称・通称(登録されている名前)で検索可能(例：イプべ、イプベタなど)\n' +
-            '`/名前検索` - 正式名称・通称の「部分一致」で検索＆性能表示(「アルファ」、「私服」、「の」など)\n' +
-            '`/性能検索` - 奥義/特技/特殊/魔道具などに含まれる効果の「部分一致」で検索＆性能表示(「出血」、「防御」、「さらに」など)\n' +
-            '`/ポジション順` - ポジション値で降順に表示\n' +
-            '`/絞り込み` - 属性、ロール条件で検索＆性能表示',
+          name: '/キャラ検索',
+          value: '正式名称・通称(登録されている名前)で検索可能(例：イプべ、イプベタなど)\n\u200B',
+        },
+        {
+          name: '/名前検索',
+          value: '正式名称・通称の「部分一致」で検索＆性能表示(「アルファ」、「私服」、「の」など)\n\u200B',
+        },
+        {
+          name: '/性能検索',
+          value: '奥義/特技/特殊/魔道具などに含まれる効果の「部分一致」で検索＆性能表示(「出血」、「防御」、「さらに」など)\n\u200B',
+        },
+        {
+          name: '/ポジション順',
+          value: 'ポジション値で降順に表示\n\u200B',
+        },
+        {
+          name: '/絞り込み',
+          value: '属性、ロール条件で検索＆性能表示\n\u200B',
         }
       );
 
-    await interaction.reply({ embeds: [embed], ephemeral: true }); // 自分だけに表示
+    await interaction.reply({ embeds: [embed], ephemeral: true });
   }
 };
